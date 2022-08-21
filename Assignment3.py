6fb4dc3cb38d1daa221cbd8806916e1226554456
Satarupa <satarupasinha.me@gmail.com>
2022-08-21 19:59:42 +0530

    Adding Assignment 3

---
+++ Assignment3.py
@@ -0,0 +1,38 @@
+#Answer 1
+str = "4"
+int(str)
+
+#Answer 2
+ch='m'
+ord(ch)
+
+#Answer 3
+x = 100
+chr(x)
+
+#Answer 4
+print(x , bin(x))
+
+#Answer 5
+print(x , oct(x))
+
+#Answer 6
+print(x , hex(x))
+
+#Answer 7
+y = 0b1100101
+print(y)
+
+#Answer 8
+w = 0x2F
+print(w)
+
+#Answer 9
+z = 0o125
+print(z)
+
+#Answer 10
+a = 0o25
+b = 0x39
+print(bin(a+b))
+

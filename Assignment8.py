13aab184b9ada53ab43e6eea1458dc68e1d57ff8
Satarupa <satarupasinha.me@gmail.com>
2022-08-23 14:23:42 +0530

    Assignment 8

---
+++ Assignment8.py
@@ -0,0 +1,73 @@
+#Answer 1
+i =0
+while i<5:
+    print("MySirG")
+    i = i+1
+
+#Answer 2
+i =1
+while i<=10:
+    print(i, end=' ')
+    i=i+1
+print()
+
+#Answer 3
+i = 0
+while i<10:
+    print(10-i, end =' ')
+    i=i+1
+print()
+
+#Answer 4
+i = 0
+while i<20:
+    if i%2 !=0:
+        print(i, end=' ')
+    i=i+1
+print()
+
+#Answer 5
+i = 20
+while i>0:
+    if i%2 !=0:
+        print(i, end=' ')
+    i=i-1
+print()
+
+#Answer 6
+i = 1
+while i<=20:
+    if i%2 ==0:
+        print(i, end=' ')
+    i=i+1
+print()
+
+#Answer 7
+i = 20
+while i>0:
+    if i%2 ==0:
+        print(i, end=' ')
+    i=i-1
+print()
+
+
+#Answer 8
+i =1
+while i<=10:
+    print(pow(i,2), end=' ')
+    i=i+1
+print()
+
+#Answer 9
+i =1
+while i<=10:
+    print(pow(i,3), end=' ')
+    i=i+1
+print()
+
+#Answer 10
+i =1
+while i<=10:
+    print(i*5, end=' ')
+    i=i+1
+print()

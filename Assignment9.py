635aefd62dd0cb82ad7a6c6a1c394030e40acf9a
Satarupa <satarupasinha.me@gmail.com>
2022-08-23 14:30:52 +0530

    Assignment 10

---
+++ Assignment9.py
@@ -0,0 +1,83 @@
+#Answer 1
+x = int(input("Enter the number of times you wish to print: "))
+i =0
+while i<x:
+    print("MySirG")
+    i = i+1
+
+#Answer 2
+x = int(input("Enter the number: "))
+i =1
+while i<=x:
+    print(i, end=' ')
+    i=i+1
+print()
+
+#Answer 3
+x = int(input("Enter the number: "))
+i = 0
+while i<x:
+    print(x-i, end =' ')
+    i=i+1
+print()
+
+#Answer 4
+x = int(input("Enter the number: "))
+i = 1
+while i<=x:
+    if i%2 !=0:
+        print(i, end=' ')
+    i=i+1
+print()
+
+#Answer 5
+x = int(input("Enter the number: "))
+i = x
+while i>0:
+    if i%2 !=0:
+        print(i, end=' ')
+    i=i-1
+print()
+
+#Answer 6
+x = int(input("Enter the number: "))
+i = 1
+while i<=x:
+    if i%2 ==0:
+        print(i, end=' ')
+    i=i+1
+print()
+
+#Answer 7
+x = int(input("Enter the number: "))
+i = x
+while i>0:
+    if i%2 ==0:
+        print(i, end=' ')
+    i=i-1
+print()
+
+
+#Answer 8
+x = int(input("Enter the number: "))
+i =1
+while i<=x:
+    print(pow(i,2), end=' ')
+    i=i+1
+print()
+
+#Answer 9
+x = int(input("Enter the number: "))
+i =1
+while i<=x:
+    print(pow(i,3), end=' ')
+    i=i+1
+print()
+
+#Answer 10
+x = int(input("Enter the number: "))
+i =1
+while i<=x:
+    print(i*5, end=' ')
+    i=i+1
+print()
